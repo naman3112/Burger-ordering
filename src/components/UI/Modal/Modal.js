081aa23ce0dc11c678a9c9d3baf51b9978ba9b0f
Naman <namanarora311@gmail.com>
2021-01-30 16:22:07 +0530

    added functionality of backdrop (opening and closing of Modals)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import classes from './Modal.module.css';
-
+import Aux from "../../../hoc/Aux"
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal=(props)=>(
-    <div className={classes.Modal} 
+   <Aux>
+       <Backdrop show={props.show} clicked={props.modalClosed}/>
+       <div className={classes.Modal} 
         style={{
             transform: props.show ? 'translateY(0)' : 'translateY(-100vh)' ,
             opacity: props.show ? '1' : '0'    
@@ -12,5 +15,8 @@ const modal=(props)=>(
       
         {props.children}
     </div>
+
+   </Aux>
+    
 )
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,11 @@ class BurgerBuilder extends Component{
     purchaseHandler=()=>{
         this.setState({purchasing: true});
     }
+    purchaseCancelHandler=()=>{
+        this.setState({purchasing: false});
+    }
+
+
     render(){
         const disabledInfo={
             ...this.state.ingredients
@@ -84,7 +89,8 @@ class BurgerBuilder extends Component{
         }
         return(
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing}
+                        modalClosed={this.purchaseCancelHandler}>
                 <OrderSummary ingredients={this.state.ingredients}/>
 
                 </Modal>
