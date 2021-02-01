b3952718072daf6267bb69ae9348ea9a568c20a9
Naman <namanarora311@gmail.com>
2021-02-01 17:31:27 +0530

    improved performance by adding lifecycle hooks like shouldComponet-Update in order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
-const orderSummary=(props)=>{
-   const ingredientSummary=Object.keys(props.ingredients) 
-    .map(igKey=>{
-        return <li key={igKey}> <span  style={{textTransform: 'capitalize'}}>{igKey} : {props.ingredients[igKey]}</span></li>
-    })
-    return(
-        <Aux>
+
+class OrderSummary extends Component{
+   componentDidUpdate(){
+       console.log("'[ordersummary will update")
+   }
+    render(){
+        const ingredientSummary=Object.keys(this.props.ingredients) 
+        .map(igKey=>{
+            return <li key={igKey}> <span  style={{textTransform: 'capitalize'}}>{igKey} : {this.props.ingredients[igKey]}</span></li>
+        })
+
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p> A delicious burger with following ingredients: </p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Aux>    
+        );
+    }
 }
-export default orderSummary;
+
+
+
+
+export default OrderSummary;
