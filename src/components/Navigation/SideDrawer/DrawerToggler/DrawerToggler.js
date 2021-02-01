36bdae9ef304e82d43914da6d0d3c66d796cef4d
Naman <namanarora311@gmail.com>
2021-02-01 16:57:09 +0530

    built basic burger ordering App

--- src/components/Navigation/SideDrawer/DrawerToggler/DrawerToggler.js
+++ src/components/Navigation/SideDrawer/DrawerToggler/DrawerToggler.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggler.module.css'
 
 const drawerToggle=(props)=>(
-    <div onClick={props.clicked} >MENU</div>
-)
+    <div className={classes.DrawerToggle} onClick={props.clicked} >
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+    )
 export default drawerToggle;
